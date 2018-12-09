95cec473b865815a601f7be682d72740c5ba25c2
Kostyantyn Krakovych <kkrakovych@icloud.com>
2018-12-09 16:25:22 +0200

    Added some unit tests.

---
+++ src/test/java/net/kosto/configuration/model/oracle/OracleScriptTest.java
@@ -0,0 +1,94 @@
+/*
+ * Copyright 2018 Kostyantyn Krakovych
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.kosto.configuration.model.oracle;
+
+import static net.kosto.util.Error.MISSING_ATTRIBUTE;
+import static net.kosto.util.FileUtils.FILE_MASK_SQL;
+import static net.kosto.util.FileUtils.UNIX_SEPARATOR;
+import static net.kosto.util.StringUtils.AMPERSAND;
+import static net.kosto.util.StringUtils.ORACLE_SCHEMA_SCRIPT_CONDITION;
+import static net.kosto.util.StringUtils.ORACLE_SCHEMA_SCRIPT_TYPE;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.nio.file.Paths;
+
+import net.kosto.configuration.model.DatabaseItem;
+import net.kosto.configuration.model.DatabaseScriptCondition;
+import net.kosto.configuration.model.DatabaseScriptType;
+import net.kosto.util.FileUtils;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+
+class OracleScriptTest {
+
+  private DatabaseItem script;
+
+  @BeforeEach
+  void setUp() {
+    script = new OracleScript();
+    script.setSourceDirectoryFull(FileUtils.ROOT_PATH);
+    script.setOutputDirectoryFull(FileUtils.ROOT_PATH);
+    script.setType(DatabaseScriptType.ONE_TIME.name());
+    script.setCondition(DatabaseScriptCondition.BEFORE.name());
+  }
+
+  @Test
+  @DisplayName("Script - Missing attribute Type.")
+  void test01() {
+    script.setType(null);
+
+    Executable executable = script::validate;
+    Throwable result = assertThrows(MojoExecutionException.class, executable);
+    assertEquals(MISSING_ATTRIBUTE.message(ORACLE_SCHEMA_SCRIPT_TYPE), result.getMessage());
+  }
+
+  @Test
+  @DisplayName("Script - Missing attribute Condition.")
+  void test02() {
+    script.setCondition(null);
+
+    Executable executable = script::validate;
+    Throwable result = assertThrows(MojoExecutionException.class, executable);
+    assertEquals(MISSING_ATTRIBUTE.message(ORACLE_SCHEMA_SCRIPT_CONDITION), result.getMessage());
+  }
+
+  @Test
+  @DisplayName("Default values.")
+  void test03() throws MojoExecutionException {
+    script.validate();
+
+    assertNull(script.getIndex());
+    assertNull(script.getOrder());
+    assertNull(script.getName());
+    assertEquals(DatabaseScriptType.ONE_TIME.name(), script.getType());
+    assertEquals(DatabaseScriptCondition.BEFORE.name(), script.getCondition());
+    assertEquals(FILE_MASK_SQL, script.getFileMask());
+    assertEquals(DatabaseScriptType.ONE_TIME.getSourceDirectory(), script.getSourceDirectory());
+    assertFalse(script.getIgnoreDirectory());
+    assertEquals(AMPERSAND, script.getDefineSymbol());
+    assertFalse(script.getIgnoreDefine());
+    assertEquals(UNIX_SEPARATOR + DatabaseScriptType.ONE_TIME.getSourceDirectory() + UNIX_SEPARATOR, script.getExecuteDirectory());
+    assertEquals(Paths.get(UNIX_SEPARATOR, DatabaseScriptType.ONE_TIME.getSourceDirectory()), script.getSourceDirectoryFull());
+    assertEquals(Paths.get(UNIX_SEPARATOR, DatabaseScriptType.ONE_TIME.getSourceDirectory()), script.getOutputDirectoryFull());
+  }
+}
